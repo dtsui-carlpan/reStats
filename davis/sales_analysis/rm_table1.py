f680f3f4cec4ef136f94ed3e3a32f876afdfef97
davistsui <davistsui218@hotmail.com>
2015-06-26 23:13:17 -0500

    are you serious

---
+++ davis/sales_analysis/rm_table1.py
@@ -0,0 +1,32 @@
+import os
+from os import listdir
+
+# running this file can rename every csv file in the data/month_year folder whose name contains 'Table 1',
+# an unfortuante name that numbers decides to add onto the output file name
+
+
+def find_csv_filenames(path_to_dir, suffix):
+    filenames = listdir(path_to_dir)
+    return [filename for filename in filenames if filename.endswith(suffix)]
+
+def clean_csv_filename():
+	# . specifies current directory
+	folders = listdir('data/')
+
+	suffix = '.csv'
+	for folder in folders:
+		path_to_dir = 'data/' + folder + '/'
+		csv_filenames = find_csv_filenames(path_to_dir, suffix)
+
+		for csv_filename in csv_filenames:
+			print csv_filename
+			if 'Table' in csv_filename:
+				# a space is represented differently in the command line
+				old_csvfile_name = path_to_dir + csv_filename.split('-')[0] + '-Table\ 1.csv'
+				new_csv_filename = path_to_dir + csv_filename.split('-')[0] + '.csv'
+				script = 'mv ' + old_csvfile_name + ' ' + new_csv_filename
+				print script
+				os.system(script)
+
+if __name__ == '__main__':
+	clean_csv_filename()
